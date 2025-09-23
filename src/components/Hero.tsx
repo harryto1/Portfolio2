9e3a51292cb3c1fe61e8a2956fa8f592b1fde9ba
harryto1 <harry.ruiz6@upr.edu>
2025-09-23 18:09:00 -0400

    Update rootMargin in IntersectionObserver for improved visibility detection

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero: React.FC = () => {
       },
       { 
         threshold: getThreshold(),
-        rootMargin: '50px 0px 50px 0px'
+        rootMargin: '0px 0px 50px 0px'
       }
     );
 
